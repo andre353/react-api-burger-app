15f4db333df24a4c1bfd230a9f6ff1be655f1da6
andre353 <eva595eva@gmail.com>
2023-02-23 21:20:32 +0300

    Fetch category data from server

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import _ from './Navigation.module.scss';
 import cn from 'classnames';
 import Container from '../Container/Container';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeCategory } from '../../store/category/categorySlice';
+import { categoryRequestAsync, changeCategory } from '../../store/category/categorySlice';
+import { API_URI } from '../../consts';
 
 const Navigation = () => {
   const {category, activeCategory} = useSelector((state) => state.category)
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(categoryRequestAsync())
+  }, [])
+
   return (
     <nav className={_.navigation}>
       <Container>
@@ -15,13 +21,13 @@ const Navigation = () => {
           <ul className={_.navigation__list}>
             {
               category.map((item, i) => (
-                <li className={_.navigation__item} key={i}>
+                <li className={_.navigation__item} key={item.title}>
                   <button 
                     className={cn(
                       _.navigation__button,
                       activeCategory === i ? _.navigation__button_active : ''
                     )}
-                    style={{backgroundImage: `url(${item.image})`}}
+                    style={{backgroundImage: `url(${API_URI}${item.image})`}}
                     onClick={() => {
                       dispatch(changeCategory({indexCategory: i}))
                     }}
