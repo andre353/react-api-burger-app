a448ad8979dbef13c9bcdd826673061dd9c638db
andre353 <eva595eva@gmail.com>
2023-02-28 20:15:18 +0300

    Add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Catalog from './components/Catalog/Catalog';
 import Header from './components/Header/Header';
 import Navigation from './components/Navigation/Navigation';
 import { ModalDelivery } from './components/ModalDelivery/ModalDelivery';
+import Footer from './components/Footer/Footer';
 
 export const App = () => {
 
@@ -12,7 +13,7 @@ export const App = () => {
         <Navigation />
         <Catalog />
       </main>
-      <footer></footer>
+      <Footer />
       <ModalDelivery />
     </>
   )
